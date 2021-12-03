f5512776fae31a949ea6927b1b693d3c242c0b18
felipeoliveira92 <feliperabelo.oliveira@gmail.com>
2021-12-03 16:29:58 -0300

    Adicionar arquivos de projeto.

---
+++ Salon.Model/EnumTipo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salon.Model
+{
+    public enum EnumTipo
+    {
+        User = 1,
+        Administrator = 2,
+        Client = 3,
+    }
+}
