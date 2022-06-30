a9b56006fd8baac82d6198dac7db589e4c951b73
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2022-06-29 22:43:05 -0300

    Add method Get in UserController

---
+++ Salon.Api/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Salon.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Salon.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetUser(string login)
+        {
+            if(!string.IsNullOrEmpty(login))
+            {
+                if(_userRepository.GetUserByLogin(login) != null)
+                {
+                    return Ok();
+                }
+            }
+            return NotFound();
+        }
+    }
+}
