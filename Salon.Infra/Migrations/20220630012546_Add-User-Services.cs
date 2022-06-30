8daa2d211e0f58f0874bbe9ae15eff414fe5626d
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2022-06-29 22:29:32 -0300

    Create Migration User-services and started api

---
+++ Salon.Infra/Migrations/20220630012546_Add-User-Services.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Salon.Infra.Migrations
+{
+    public partial class AddUserServices : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    IdService = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameService = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DescriptionService = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PriceService = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.IdService);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    IdUser = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameUser = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LoginUser = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PasswordUser = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.IdUser);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Salon.Model/Models/Schedule.cs
+++ Salon.Model/Models/Schedule.cs
@@ -10,7 +10,9 @@ namespace Salon.Model.Models
         public DateTime DateInitial { get; set; }
         public DateTime DateEnd { get; set; }
         public int IdService { get; set; }
+        public Services Service { get; set; }
         public int IdUser { get; set; }
+        public User User { get; set; }
         public decimal Price { get; set; }
         public string Observation { get; set; }
     }
